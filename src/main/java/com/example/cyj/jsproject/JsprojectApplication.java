a91751c4d0159e297b8af86df59412b7ad642cdc
eqvyoo <ys07106@gmail.com>
2023-05-08 23:22:00 +1000

    initial commit

---
+++ src/main/java/com/example/cyj/jsproject/JsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyj.jsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cyj/jsproject/JsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyj.jsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
